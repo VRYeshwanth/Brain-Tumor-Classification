ff8ebcad3b0cb844e5fcad1c7b6a24a757de291a
VR Yeshwanth <vryeshwanth06@gmail.com>
2026-09-19 11:12:15 +0530

    Identify misclassified classes in the test set evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,6 +6,7 @@ from torchmetrics.classification import (
 )
 import torch
 import matplotlib.pyplot as plt
+import math
 
 def evaluateModel(model, dataloader, device):
 
@@ -57,6 +58,79 @@ def plot_confusion_matrix(cm, class_names, title):
         ax.get_yticklabels(),
         rotation=0
     )
+
+    plt.tight_layout()
+    plt.show()
+
+
+def get_misclassified_images(model, dataloader, dataset, device, class_names):
+    model.eval()
+
+    misclassified = []
+    index = 0
+
+    with torch.no_grad():
+
+        for images, labels in dataloader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            probabilities = torch.softmax(outputs, dim=1)
+
+            confidences, predictions = torch.max(
+                probabilities, dim=1
+            )
+
+            for i in range(len(images)):
+
+                if predictions[i] != labels[i]:
+                    misclassified.append({
+                        "image": images[i].cpu(),
+                        "true_label": class_names[labels[i].item()],
+                        "predicted_label": class_names[predictions[i].item()],
+                        "confidence": confidences[i].item(),
+                        "path": dataset.samples[index][0]
+                    })
+
+                index += 1
+    return misclassified
+
+
+mean = torch.tensor([0.485, 0.456, 0.406])
+std = torch.tensor([0.229, 0.224, 0.225])
+
+def show_misclassified_images(misclassified, n=20):
+
+    n = min(n, len(misclassified))
+
+    rows = math.ceil(n / 5)
+
+    plt.figure(figsize=(15, rows * 3))
+
+    for i in range(n):
+
+        item = misclassified[i]
+
+        image = item["image"]
+        image = image * std[:, None, None] + mean[:, None, None]
+
+        image = image.permute(1, 2, 0)
+        image = image.clamp(0, 1)
+
+        plt.subplot(rows, 5, i + 1)
+        plt.imshow(image)
+
+        plt.title(
+            f"True: {item['true_label']}\n"
+            f"Pred: {item['predicted_label']}\n"
+            f"Conf: {item['confidence']:.2%}"
+        )
+
+        plt.axis("off")
+
+    plt.suptitle("Misclassified Test Images", fontsize=20)
 
     plt.tight_layout()
     plt.show()
